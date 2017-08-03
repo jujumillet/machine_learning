759b76f2b1533ff77095df243a5a4faf4b21ab58
jujumillet <mj1fender@gmail.com>
2017-08-04 00:06:50 +0100

    premier script machine learning

---
+++ MorF.py
@@ -0,0 +1,51 @@
+from sklearn import tree
+
+#[height, weight, shoe size]
+X = [
+	[181, 80, 44],
+	[177, 70, 43],
+	[160, 60, 38],
+	[154, 54, 37],
+	[166, 65, 40],
+	[190, 90, 47],
+	[175, 64, 39],
+	[177, 70, 40],
+	[159, 55, 37],
+	[171, 75, 42],
+	[181, 85, 43],
+	[173, 75, 43],
+	[176, 62, 42],
+	[156, 62, 37],
+	[163, 55, 39],
+	[180, 72, 43],
+	[187, 98, 45]
+	]
+
+Y = [
+	 'male',
+     'male', 
+     'female', 
+     'female', 
+     'male', 
+     'male', 
+     'female', 
+     'female',
+     'female', 
+     'male', 
+     'male', 
+     'male', 
+     'male', 
+     'female', 
+     'female', 
+     'male', 
+     'male'
+     ]
+
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[187, 98, 45]])
+
+print (prediction)
